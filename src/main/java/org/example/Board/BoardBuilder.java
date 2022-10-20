c0559ed2d113ae89bbcb5dcd261aab23e70a7d12
MC <M@>
2022-10-20 21:36:36 +0200

    refactored boardBuilder

--- src/main/java/org/example/Board/BoardBuilder.java
+++ src/main/java/org/example/Board/BoardBuilder.java
@@ -4,12 +4,6 @@ import org.example.Figures.Figure;
 
 public class BoardBuilder {
 
-    Board board;
-
-    public BoardBuilder(Board board) {
-        this.board = board;
-    }
-
     final String lowercaseRED = "\u001B[31m";
     final String uppercaseBLUE = "\u001B[34m";
     final String COLOR_RESET = "\u001B[0m";
@@ -17,6 +11,16 @@ public class BoardBuilder {
     final String horizontalSeparator = "   +-----+-----+-----+-----+-----+-----+-----+-----+\n";
     final String letters = "      a     b     c     d     e     f     g     h";
     final String newLine = "\n";
+    Board board;
+
+    public BoardBuilder(Board board) {
+        this.board = board;
+    }
+
+    private static void postNumberCoordinates(StringBuilder boardString, int y) {
+        boardString.append(y);
+        boardString.append("\n");
+    }
 
     public String buildUpperCaseBoard() {
         StringBuilder boardString = new StringBuilder();
@@ -24,30 +28,8 @@ public class BoardBuilder {
         boardString.append(letters);
         boardString.append(newLine);
         boardString.append(horizontalSeparator);
-        for (int y = board.getBoard().length - 1; y >= 0; y--) {
-            boardString.append(y);
-            boardString.append(space);
-            boardString.append(space);
-            boardString.append("┊  ");
-            for (int x = 0; x < board.getBoard()[0].length; x++) {
-                if (boardArr[y][x] == null) {
-                    boardString.append(space);
-                } else if (boardArr[y][x] instanceof Figure) {
-                    if (((Figure) boardArr[y][x]).isLowercase()) {
-                        boardString.append(lowercaseRED);
-                        boardString.append(boardArr[y][x].toString());
-                        boardString.append(COLOR_RESET);
-                    } else {
-                        boardString.append(uppercaseBLUE);
-                        boardString.append(boardArr[y][x].toString());
-                        boardString.append(COLOR_RESET);
-                    }
-                }
-                boardString.append("  ┊  ");
-            }
-            boardString.append(y);
-            boardString.append("\n");
-            boardString.append(horizontalSeparator);
+        for (int y = 7; y >= 0; y--) {
+            buildRow(boardString, boardArr, y);
         }
         boardString.append(letters);
         return boardString.toString();
@@ -59,32 +41,44 @@ public class BoardBuilder {
         boardString.append(letters);
         boardString.append(newLine);
         boardString.append(horizontalSeparator);
-        for (int y = 0; y < board.getBoard().length; y++) {
-            boardString.append(y);
-            boardString.append(space);
-            boardString.append(space);
-            boardString.append("┊  ");
-            for (int x = 0; x < board.getBoard()[0].length; x++) {
-                if (boardArr[y][x] == null) {
-                    boardString.append(space);
-                } else if (boardArr[y][x] instanceof Figure) {
-                    if (((Figure) boardArr[y][x]).isLowercase()) {
-                        boardString.append(lowercaseRED);
-                        boardString.append(boardArr[y][x].toString());
-                        boardString.append(COLOR_RESET);
-                    } else {
-                        boardString.append(uppercaseBLUE);
-                        boardString.append(boardArr[y][x].toString());
-                        boardString.append(COLOR_RESET);
-                    }
-                }
-                boardString.append("  ┊  ");
-            }
-            boardString.append(y);
-            boardString.append("\n");
-            boardString.append(horizontalSeparator);
+        for (int y = 0; y <= 7; y++) {
+            buildRow(boardString, boardArr, y);
         }
         boardString.append(letters);
         return boardString.toString();
+    }
+
+    private void buildRow(StringBuilder boardString, Object[][] boardArr, int y) {
+        preNumberCoordinates(boardString, y);
+        buildField(boardString, boardArr, y);
+        postNumberCoordinates(boardString, y);
+        boardString.append(horizontalSeparator);
+    }
+
+    private void buildField(StringBuilder boardString, Object[][] boardArr, int y) {
+        for (int x = 0; x < board.getBoard()[0].length; x++) {
+            if (boardArr[y][x] == null) {
+                boardString.append(space);
+            } else if (boardArr[y][x] instanceof Figure) {
+                String figureSymbol = boardArr[y][x].toString();
+                if (((Figure) boardArr[y][x]).isLowercase()) {
+                    boardString.append(lowercaseRED);
+                    boardString.append(figureSymbol);
+                    boardString.append(COLOR_RESET);
+                } else {
+                    boardString.append(uppercaseBLUE);
+                    boardString.append(figureSymbol);
+                    boardString.append(COLOR_RESET);
+                }
+            }
+            boardString.append("  ┊  ");
+        }
+    }
+
+    private void preNumberCoordinates(StringBuilder boardString, int y) {
+        boardString.append(y);
+        boardString.append(space);
+        boardString.append(space);
+        boardString.append("┊  ");
     }
 }
